88c38a848e80df957b90511110569da7949878e7
chun-wan <chun-wan@amd.com>
2020-02-20 07:48:51 -0500

    [lincs] add device code check
    (cherry picked from commit 626b11a1c72fd6448bef72c9d84b6cb53843814c)

--- src/gromacs/mdlib/lincs_cuda.hip.cpp
+++ src/gromacs/mdlib/lincs_cuda.hip.cpp
@@ -76,12 +76,17 @@
 #include "gromacs/topology/ifunc.h"
 #include "gromacs/topology/topology.h"
 
-#define GMX_ATOMIC_OVERWRITE
+//To check whether the gpu support atomicadd by device code
+static bool gcnArchSupportAtomicFloat(void){
+	hipDeviceProp_t prop; 
+	hipGetDeviceProperties(&prop, 0);
+	if( prop.gcnArch > 906 )	
+		return true;
+	else
+		return false;
+}
 
-
-
-#ifdef GMX_ATOMIC_OVERWRITE
- __device__ __forceinline__  void atomicAddOverWriteForFloat(const float* __restrict__ address,const float val) {
+__device__ __forceinline__  void atomicAddOverWriteForFloat(const float* __restrict__ address,const float val) {
   const int* address_as_ull = (int*)address;
   int old = *address_as_ull;
   int assumed;
@@ -101,7 +106,6 @@
     atomicAddOverWriteForFloat(&address->z, val.z);
 
 }
-#endif
 
 namespace gmx
 {
@@ -112,7 +116,8 @@ namespace gmx
 constexpr static int c_threadsPerBlock = 512; 
 //! Maximum number of threads in a block (for __launch_bounds__)
 constexpr static int c_maxThreadsPerBlock = c_threadsPerBlock;
-
+//! Check GCN arch of GPU
+static bool gcn=gcnArchSupportAtomicFloat();
 /*! \brief Main kernel for LINCS constraints.
  *
  * See Hess et al., J. Comput. Chem. 18: 1463-1472 (1997) for the description of the algorithm.
@@ -287,16 +292,13 @@ __launch_bounds__(c_maxThreadsPerBlock) __global__
     // Writing for all but dummy constraints
     if (!isDummyThread)
     {
-	#ifdef GMX_ATOMIC_OVERWRITE
-		atomicAddOverWriteForFloat3(&gm_xp[i], -tmp * inverseMassi);
-	        atomicAddOverWriteForFloat3(&gm_xp[j], tmp * inverseMassj);
-	#else
-
+	if(gcn){
                 atomicAdd(&gm_xp[i], -tmp * inverseMassi);
                 atomicAdd(&gm_xp[j], tmp * inverseMassj);
-
-	#endif
-
+	}else{
+		atomicAddOverWriteForFloat3(&gm_xp[i], -tmp * inverseMassi);
+	        atomicAddOverWriteForFloat3(&gm_xp[j], tmp * inverseMassj);
+	}
     }
 
     /*
@@ -363,13 +365,13 @@ __launch_bounds__(c_maxThreadsPerBlock) __global__
         if (!isDummyThread)
         {
             float3 tmp = rc * sqrtmu_sol;
-	    #ifdef GMX_ATOMIC_OVERWRITE
-		    atomicAddOverWriteForFloat3(&gm_xp[i], -tmp * inverseMassi);
-		    atomicAddOverWriteForFloat3(&gm_xp[j], tmp * inverseMassj);
-	    #else
+	    if(gcn){
 	            atomicAdd(&gm_xp[i], -tmp * inverseMassi);
 	            atomicAdd(&gm_xp[j], tmp * inverseMassj);
-	    #endif
+   	    }else{
+		    atomicAddOverWriteForFloat3(&gm_xp[i], -tmp * inverseMassi);
+		    atomicAddOverWriteForFloat3(&gm_xp[j], tmp * inverseMassj);
+	    }
         }
     }
 
@@ -377,13 +379,13 @@ __launch_bounds__(c_maxThreadsPerBlock) __global__
     if (updateVelocities && !isDummyThread)
     {
         float3 tmp = rc * invdt * lagrangeScaled;
-	#ifdef GMX_ATOMIC_OVERWRITE
-		atomicAddOverWriteForFloat3(&gm_v[i], -tmp * inverseMassi);
-		atomicAddOverWriteForFloat3(&gm_v[j], tmp * inverseMassj);
-	#else
+	if(gcn){
 	        atomicAdd(&gm_v[i], -tmp * inverseMassi);
         	atomicAdd(&gm_v[j], tmp * inverseMassj);
-	#endif
+	}else{
+		atomicAddOverWriteForFloat3(&gm_v[i], -tmp * inverseMassi);
+		atomicAddOverWriteForFloat3(&gm_v[j], tmp * inverseMassj);
+	}
     }
 
 
@@ -441,11 +443,11 @@ __launch_bounds__(c_maxThreadsPerBlock) __global__
         // First 6 threads in the block add the results of 6 tensor components to the global memory address.
         if (threadIdx.x < 6)
         {
-            	#ifdef GMX_ATOMIC_OVERWRITE
-			atomicAddOverWriteForFloat(&(gm_virialScaled[threadIdx.x]), sm_threadVirial[threadIdx.x * blockDim.x]);	
-		#else
+		if(gcn){
 			atomicAdd(&(gm_virialScaled[threadIdx.x]), sm_threadVirial[threadIdx.x * blockDim.x]);
-		#endif
+		}else{
+			atomicAddOverWriteForFloat(&(gm_virialScaled[threadIdx.x]), sm_threadVirial[threadIdx.x * blockDim.x]);	
+		}
 
         }
     }
