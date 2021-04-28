6b2759d84e96caeb9051b65f78f4379cc3c5f9c3
root <vicky.w.tsang@gmail.com>
2021-04-28 10:41:18 -0700

    bCalcFshift

--- src/gromacs/nbnxm/hip/nbnxm_cuda_kernel_hip.h
+++ src/gromacs/nbnxm/hip/nbnxm_cuda_kernel_hip.h
@@ -638,13 +638,22 @@ __launch_bounds__(THREADS_PER_BLOCK)
     }
 
     /* add up local shift forces into global mem, tidxj indexes x,y,z */
-    if (bCalcFshift && tidxj < 3)
+    if ( bCalcFshift)
     {
-	#if ((HIP_VERSION_MAJOR >= 3) && (HIP_VERSION_MINOR > 3)) || (HIP_VERSION_MAJOR >= 4)
-            atomicAddNoRet(&(atdat.fshift[nb_sci.shift].x) + tidxj, fshift_buf);
-        #else
-            atomicAddOverWriteForFloat(&(atdat.fshift[nb_sci.shift].x) + tidxj, fshift_buf);
-        #endif
+        #pragma unroll
+        for (unsigned int offset = (c_clSize >> 1); offset > 0; offset >>= 1)
+        {
+            fshift_buf += __shfl_down(fshift_buf, offset);
+        }
+
+        if( tidxi == 0 && tidxj < 3 )
+        {
+            #if (HIP_VERSION_MAJOR >= 3) && (HIP_VERSION_MINOR > 3)
+                    atomicAddNoRet(&(atdat.fshift[nb_sci.shift].x) + tidxj, fshift_buf);
+            #else
+                    atomicAddOverWriteForFloat(&(atdat.fshift[nb_sci.shift].x) + tidxj, fshift_buf);
+            #endif
+        }
     }
 
 #    ifdef CALC_ENERGIES
