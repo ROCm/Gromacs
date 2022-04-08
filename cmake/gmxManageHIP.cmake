d0d70f3003f1fb71b8999ece481d37d037e7030b
wunhuang <wunhuang@amd.com>
2022-04-08 05:27:55 -0500

    Add roctx tag to trace cpu activity

--- cmake/gmxManageHIP.cmake
+++ cmake/gmxManageHIP.cmake
@@ -32,6 +32,7 @@
 #
 # To help us fund GROMACS development, we humbly ask that you cite
 # the research papers on the package. Check out http://www.gromacs.org.
+include_directories("/opt/rocm/roctracer/include")
 
 IF (NOT DEFINED ROCM_PATH)
     IF(NOT DEFINED ENV{ROCM_PATH})
