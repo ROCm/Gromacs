c4286e89015246910ce80ecbaeeb554a45456198
Istvan Kiss <istvan@streamhpc.com>
2023-01-17 18:19:29 +0100

    [Issue] fix mpi-aware problem

--- src/gromacs/mdrun/runner.cpp
+++ src/gromacs/mdrun/runner.cpp
@@ -243,7 +243,7 @@ static DevelopmentFeatureFlags manageDevelopmentFeatures(const gmx::MDLogger& md
     // Direct GPU comm path is being used with GPU-aware MPI
     // make sure underlying MPI implementation is GPU-aware
 
-    if (GMX_LIB_MPI && (GMX_GPU_CUDA || GMX_GPU_SYCL))
+    if (GMX_LIB_MPI && (GMX_GPU_CUDA || GMX_GPU_SYCL || GMX_GPU_HIP))
     {
         // Allow overriding the detection for GPU-aware MPI
         GpuAwareMpiStatus gpuAwareMpiStatus = checkMpiCudaAwareSupport();
